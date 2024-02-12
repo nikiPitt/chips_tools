63817dd685376ff1fdea3e47734968fff360b4d6
nikiPitt <knocknocknik@gmail.com>
2024-02-12 12:00:45 -0500

    Loading DAT files commit

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -1,8 +1,9 @@
-import cc_dat_utils
+import cc_dat_utils as ccut
 
 #Part 1
 input_dat_file = "data/pfgd_test.dat"
 
+def main():
+    data = ccut.make_cc_level_pack_from_dat(input_dat_file)
 
-#Use cc_dat_utils.make_cc_level_pack_from_dat() to load the file specified by input_dat_file
-#print the resulting data
+main()
