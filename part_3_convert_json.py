3d6b0630438404a7b9f21ca14b0aacede97a2fee
nikiPitt <knocknocknik@gmail.com>
2024-03-01 17:53:29 -0500

    An update to meet a better editor data json data

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -12,40 +12,41 @@ def make_level_pack_from_json(json_data):
     # Create GameLibrary object
     level_pack = cc_classes.CCLevelPack()
 
+    allLevelData = json_data['levels']
+
     # Iterate through json file
-    for key in json_data:
+    for levelData in allLevelData:
         level = cc_classes.CCLevel()
 
         # Inititate mandatory fields
-        currentlevel = json_data[key]
-        level.level_number = currentlevel["level_number"]
-        level.time = currentlevel["time"]
-        level.num_chips = currentlevel["chip_number"]
-        level.upper_layer = currentlevel["upper_layer"]
+        level.level_number = levelData["level_number"]
+        level.time = levelData["time"]
+        level.num_chips = levelData["numberOfChips"]
+        level.upper_layer = levelData["upperLayer"]
 
         # Initiate optional fields
-        level.add_field(cc_classes.CCMapTitleField(currentlevel["map_title"]))
-        level.add_field(cc_classes.CCEncodedPasswordField(currentlevel["password"]))
-        level.add_field(cc_classes.CCMapHintField(currentlevel["hint_text"]))
+        level.add_field(cc_classes.CCMapTitleField(levelData["map_title"]))
+        level.add_field(cc_classes.CCEncodedPasswordField(levelData["password"]))
+        level.add_field(cc_classes.CCMapHintField(levelData["hintText"]))
 
         # Check existence first and initiate the object
         # CLass MonsterMovementField
-        if len(currentlevel["monsters"]) != 0:
-            level.add_field(cc_classes.CCMonsterMovementField([cc_classes.CCCoordinate(currentlevel["monsters"][0][0],
-                                                                                       currentlevel["monsters"][0][1])]))
+        if len(levelData["monsters"]) != 0:
+            level.add_field(cc_classes.CCMonsterMovementField([cc_classes.CCCoordinate(levelData["monsters"][0][0],
+                                                                                       levelData["monsters"][0][1])]))
         # Class TrapControlField
-        if len(currentlevel["brown_buttons"]) != 0:
-            level.add_field(cc_classes.CCTrapControlsField([cc_classes.CCTrapControl(currentlevel["brown_buttons"][0][0],
-                                                                                     currentlevel["brown_buttons"][0][1],
-                                                                                     currentlevel["brown_buttons"][0][2],
-                                                                                     currentlevel["brown_buttons"][0][3])]))
+        if ("brown_buttons" in levelData and (levelData["brown_buttons"]) != 0):
+            level.add_field(cc_classes.CCTrapControlsField([cc_classes.CCTrapControl(levelData["brown_buttons"][0][0],
+                                                                                     levelData["brown_buttons"][0][1],
+                                                                                     levelData["brown_buttons"][0][2],
+                                                                                     levelData["brown_buttons"][0][3])]))
         # Class CloningMachineControlsField
-        if len(currentlevel["red_buttons"]) != 0:
+        if ("red_buttons" in levelData and len(levelData["red_buttons"]) != 0):
             level.add_field(cc_classes.CCCloningMachineControlsField([cc_classes.CCCloningMachineControl(
-                                                                                     currentlevel["red_buttons"][0][0],
-                                                                                     currentlevel["red_buttons"][0][1],
-                                                                                     currentlevel["red_buttons"][0][2],
-                                                                                     currentlevel["red_buttons"][0][3])]))
+                                                                                     levelData["red_buttons"][0][0],
+                                                                                     levelData["red_buttons"][0][1],
+                                                                                     levelData["red_buttons"][0][2],
+                                                                                     levelData["red_buttons"][0][3])]))
         # Make total level pack using level objects
         level_pack.levels.append(level)
 
@@ -53,13 +54,14 @@ def make_level_pack_from_json(json_data):
 
 def loadfile():
     # Part 2
-    input_json_file = "data/nnkim_cc1.json"
+    input_json_file = "data/nnkim_allLevelData.json"
     # Load json file Python wise
     with open(input_json_file, "r") as f:
         json_data = json.load(f)
     # Call make_game_library_from_json function
     level_pack = make_level_pack_from_json(json_data)
     # Make level pack into dat file
-    cc_dat_utils.write_cc_level_pack_to_dat(level_pack, "data/nnkim_cc1.dat")
+    cc_dat_utils.write_cc_level_pack_to_dat(level_pack, "data/nnkim_allLevelData.dat")
+    print(level_pack);
 
 loadfile()
